4a194833ec738899c7ae8552b51279ddea0b8db9
lilu <lilu@miteke.com>
2017-05-08 18:10:56 +0800

    server log

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const zmq = require('zeromq')
 const sock = zmq.socket('rep');
 sock.bind(process.argv[2] || "tcp://127.0.0.1:7777");
 sock.on("message", function(data) {
+    console.log("recv: " + data)
     sock.send(data + " back");
 });
